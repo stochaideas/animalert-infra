a86b58e006c6c953eb857e8dd744581749854b11
testfidelitycore <128906205+testfidelitycore@users.noreply.github.com>
2025-04-27 15:07:55 +0300

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -78,7 +78,10 @@ resource "aws_subnet" "public" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0" gateway_id = aws_internet_gateway.igw.id }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   tags = { Name = "animalert-public-rt" }
 }
 
@@ -113,7 +116,10 @@ resource "aws_subnet" "private" {
 resource "aws_route_table" "private" {
   count  = length(aws_subnet.private)
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0" nat_gateway_id = aws_nat_gateway.nat[count.index].id }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
+  }
   tags = { Name = "animalert-private-rt-${count.index + 1}" }
 }
 
