9566c2b0a5a3561314caf4a9d22244d10a1342e2
testfidelitycore <128906205+testfidelitycore@users.noreply.github.com>
2025-04-27 12:41:36 +0300

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -17,7 +17,7 @@ variable "vpc_id" {
 
 variable "public_subnets" {
   type = list(string)
-  # example: default = ["subnet-11111111", "subnet-22222222"]
+  example: default = ["subnet-0eb9d9ee655898647", "subnet-0458ee3a121ef69d2"]
 }
 
 variable "private_subnets" {
