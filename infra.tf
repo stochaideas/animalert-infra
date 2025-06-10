fea21208354220d2e228a8ea16ad8da7c4a86a0c
testfidelitycore <128906205+testfidelitycore@users.noreply.github.com>
2025-06-10 09:37:19 +0300

    Update infra.tf (#24)

--- infra.tf
+++ infra.tf
@@ -281,6 +281,15 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+   # Allow HTTP
+  ingress {
+    description = "HTTP in"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
