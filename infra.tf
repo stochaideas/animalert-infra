25f57365c142ca46064d758a0ab251b10a780953
testfidelitycore <128906205+testfidelitycore@users.noreply.github.com>
2025-06-22 11:29:15 -0400

    Update infra.tf (#26)
    * Update infra.tf
    
    * Update infra.tf

--- infra.tf
+++ infra.tf
@@ -989,7 +989,7 @@ resource "aws_wafv2_web_acl_association" "alb_waf_assoc" {
 
  resource "aws_wafv2_web_acl_logging_configuration" "alb_waf_logging" {
    resource_arn            = aws_wafv2_web_acl.alb_waf.arn
-   log_destination_configs = [aws_s3_bucket.logs.arn]
+   log_destination_configs = ["${aws_s3_bucket.logs.arn}"]
 }
 
 
