f3450e2f767f481b72827c1155c790cd6dd29919
testfidelitycore <128906205+testfidelitycore@users.noreply.github.com>
2025-07-13 13:03:28 +0300

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -300,31 +300,11 @@ resource "aws_s3_bucket" "backups_stage" {
 
 resource "aws_s3_bucket" "pdf_stage" {
   bucket = "animalert-pdfs-stage"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = aws_kms_key.s3_default.id
-      }
-    }
-  }
-
   lifecycle { prevent_destroy = true }
 }
 
 resource "aws_s3_bucket" "pdfs" {
   bucket = "animalert-pdfs-prod"
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = aws_kms_key.s3_default.id
-      }
-    }
-  }
-
   lifecycle { prevent_destroy = true }
 }
 
